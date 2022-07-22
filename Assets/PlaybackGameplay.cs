71df2829cd6c4ac718d68337ae3c3e5f9650b70b
bendzz <bendzz@live.co.uk>
2022-07-21 22:05:28 -0600

    WIP recording/playback 3
    (Gonna delete the animatedObject stuff)

--- Assets/PlaybackGameplay.cs
+++ Assets/PlaybackGameplay.cs
@@ -11,6 +11,7 @@ using System.Reflection;
 
 /// <summary>
 /// For recording and playing back gameplay for promotional purposes
+/// TODO rename to Recordings
 /// </summary>
 public class PlaybackGameplay : MonoBehaviour
 {
@@ -18,83 +19,156 @@ public class PlaybackGameplay : MonoBehaviour
     public List<Transform> transforms;
     public List<Ghost> ghosts;
 
-    //[System.Serializable]
-    //public abstract class Frame
-    //{
-    //    // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/where-generic-type-constraint TODO limit types
-    //    public float time;
-    //    //public Frame()
-    //    //{ }
-    //    //public abstract void playValue();
-    //    //public abstract void recordValue();
-    //}
-    //[System.Serializable]
-    //public class TransformFrame : Frame
-    //{
-    //    bool testTF = true;
-    //    Transform transform;
 
-    //    public TransformFrame(Transform tf)
+    public Clip clip;
+
+    /// <summary>
+    /// Animation clips -> AnimatedObjects -> AnimatedPropertys -> FrameData // TODO
+    /// </summary>
+    public class Clip
+    {
+        public string name;
+        //public List<AnimatedObject> objects;
+        /// <summary>
+        /// Note: The order of this has to stay constant during the game- No removing properties. 
+        /// These indices are used for matching frameData to their property during saving/loading
+        /// </summary>
+        public List<AnimatedProperty> animatedProperties;
+        //public Dictionary<int, AnimatedProperty> animatedProperties;
+
+        public Clip(string _name)
+        {
+            name = _name;
+            //objects = new List<AnimatedObject>();
+            animatedProperties = new List<AnimatedProperty>();
+            //animatedProperties = new Dictionary<int, AnimatedProperty>();
+        }
+
+        public AnimatedProperty addProperty(object obj)
+        {
+            AnimatedProperty property = new AnimatedProperty(obj, this);
+            //animatedProperties.Add(property);
+            //animatedProperties.Add(property);
+            return property;
+        }
+
+        public void saveClip()
+        {
+        }
+        public void loadClip()
+        {
+        }
+    }
+
+    //public class AnimatedObject
+    //{
+    //    /// <summary>
+    //    /// The link to the object in question. Set manually or via scene hierarchy or other methods, stored at the start of the JSON file.
+    //    /// </summary>
+    //    public object obj;
+    //    public List<AnimatedProperty> animatedProperties;
+
+    //    public AnimatedObject(object _obj)
     //    {
-    //        transform = tf;
+    //        obj = _obj;
+    //        animatedProperties = new List<AnimatedProperty>();
     //    }
 
-    //    //public override void playValue()
-    //    //{
-    //    //    throw new System.NotImplementedException();
-    //    //}
-    //    //public override void recordValue()
-    //    //{
-    //    //    throw new System.NotImplementedException();
-    //    //}
     //}
-    //[System.Serializable]
-    //public class GhostFrame : Frame
-    //{
-    //    public GhostFrame(Ghost ghos)
-    //    {
-    //        ghost = ghos;
-    //    }
-    //    float testGhost = 0;
-    //    Ghost ghost;
-    //}
+    /// <summary>
+    /// Links frameDatas to unity properties and serves as a factory for creating different FrameData types
+    /// </summary>
+    public class AnimatedProperty
+    {
+        /// <summary>
+        /// Which clip this property belongs to
+        /// </summary>
+        public Clip clip;
+        
+        /// <summary>
+        /// Corresponds to the ID in FrameData
+        /// </summary>
+        public int ID;
 
-    // TODO a comment or tooltip explaining this saving hierarchy
-    // TODO a class or method to tie frame IDs to objects in unity!
-    // "keyset"?
 
-    //[System.Serializable]
-    //public class Frame
-    //{
-    //    public int ID;
-    //    public float time;
-    //    public FrameData data;
+        //public string objString;  // JSON reference to object..? 
+        /// <summary>
+        /// In game reference to the variable/method/object etc to be recorded or animated
+        /// </summary>
+        public object obj;
 
-    //    public string ToJson()
-    //    {
-    //        return JsonUtility.ToJson(this) + data.ToJson();
-    //    }
-    //    public void loadJSON()
-    //    {
+        //public System.Type FrameType;
+        public System.Type Type;
+        public string type;
 
-    //    }
-    //}
-    //[System.Serializable]
+
+        /// <summary>
+        /// For saving and restoring Reflection gathered FieldInfo, PropertyInfo, methods etc, so those variables/methods can be recorded and animated
+        /// </summary>
+        public string propertyReference;
+
+        public List<FrameData> frames;
+
+        //public AnimatedProperty(object _property, System.Type _FrameType)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_obj">The property to be animated</param>
+        public AnimatedProperty(object _obj, Clip _clip)
+        {
+            obj = _obj;
+            clip = _clip;
+            //FrameType = _FrameType; // TODO automate this?
+
+            _clip.animatedProperties.Add(this);
+            ID = _clip.animatedProperties.Count - 1;
+
+            Type = obj.GetType();
+            type = Type.ToString();
+
+            frames = new List<FrameData>();
+
+        }
+
+        /// <summary>
+        /// Factory to make the correct frameData type
+        /// </summary>
+        public void addNewFrame(float time)
+        {
+            FrameData frame = null;
+            //frame = new (FrameType)(ID, this);  // TODO how?
+
+            if (obj is Transform)
+            {
+                frame = new TransformFrame(this, time);
+            }
+            else
+                Debug.LogError("unknown frameData object attempting to be created for: " + obj);
+
+            frames.Add(frame);
+        }
+    }
+
     // TODO: Write a far more compact string to data and data to string function that can be called by all of these 
     public abstract class FrameData
     {
+        //public string type; // TODO pretty wasteful having this in every frame
         public float time;
         public int ID;
-        public System.Type type;
+        private AnimatedProperty property;
+        //public System.Type type;
 
         //public abstract void record();
 
-        public FrameData(int _ID, float _time)
+        //public FrameData(int _ID, float _time, AnimatedProperty _property)
+        public FrameData(float _time, AnimatedProperty _property)
         {
             time = _time;
-            ID = _ID;
-            type = GetType();
+            ID = _property.ID;
+            //type = GetType();
             //type = ;
+            //type = GetType().ToString();
+            property = _property;
         }
 
         public abstract void playBack();
@@ -112,15 +186,19 @@ public class PlaybackGameplay : MonoBehaviour
     //[System.Serializable]
     public class TransformFrame : FrameData
     {
-        public Transform obj;
+        //public Transform obj;
+        public AnimatedProperty property;
 
         public Vector3 lPos;
         public Vector3 lScal;
         public Quaternion lRot;
 
-        public TransformFrame(Transform refTransform, int _ID, float _time) : base(_ID, _time)
+        //public TransformFrame(AnimatedProperty _property, int _ID, float _time) : base(_ID, _time, _property)
+        public TransformFrame(AnimatedProperty _property, float _time) : base(_time, _property)
         {
-            obj = refTransform;
+            //obj = refTransform;
+            property = _property;
+            Transform obj = (Transform)property.obj;
 
             lPos = obj.localPosition;
             lScal = obj.localScale;
@@ -134,6 +212,8 @@ public class PlaybackGameplay : MonoBehaviour
 
         public override void playBack()
         {
+            Transform obj = (Transform)property.obj;
+
             obj.localPosition = lPos;
             obj.localScale = lScal;
             obj.localRotation = lRot;
@@ -199,56 +279,24 @@ public class PlaybackGameplay : MonoBehaviour
     */
 
 
-    //public class Keyframe1<T> : Keyframe
-    //{
-    //    public T value;
-    //    public float time;
-    //}
-    //public class Keyframe<Transform> : Keyframe<T> where T : Transform
-    //{
-    //    public T value;
-    //    public float time;
-    //}
-    //public List<Frame> keyingSet;
-
-    public List<FrameData> frames;
-
     // Start is called before the first frame update
     void Start()
     {
-        //saveFile();
 
-        //keyingSet = new List<Frame>();
-        //keyingSet.Add(new TransformFrame(transform));
-        //keyingSet.Add(new GhostFrame(ghosts[0]));
+        //testReflection();
 
-        //JsonableListWrapper<Frame> jsonList = new JsonableListWrapper<Frame>(keyingSet);
-        ////string json = JsonUtility.ToJson(jsonList);
-        //string json = JsonUtility.ToJson(new GhostFrame(ghosts[0]));
-        //print(json);
+        clip = new Clip("test pacman clip");
+        AnimatedProperty property = clip.addProperty(transform);
 
-        //Frame frame = new Frame();
-        //frame.time = Time.realtimeSinceStartup;
-        //frame.data = new TransformFrame(transform);
+        property.addNewFrame(Time.time);
+        property.addNewFrame(666);
 
-        //string json = JsonUtility.ToJson(frame);
-        //FrameData frame = new TransformFrame(transform, 123, Time.realtimeSinceStartup);
-        //string json = frame.ToJson();
-        //print("JSON " + json);
+        print("JSON " + property.frames[0].ToJson());
+        print("JSON " + property.frames[1].ToJson());
 
-        //FrameData frame2 = new TransformFrame(transform, 222, 555);
-        //frame2.FromJson(frame.ToJson());
-        //string json2 = frame2.ToJson();
-        //print("JSON 2 " + json2);
-
-
-        frames = new List<FrameData>();
-        frames.Add(new TransformFrame(transform, 123, Time.realtimeSinceStartup));
-        frames.Add(new TransformFrame(transform, 5555, 6666));
-
-        saveFrames();
-
-        //loadFrames();
+        AnimatedProperty property2 = clip.addProperty(transforms[0]);
+        property2.addNewFrame(Time.time);
+        print("JSON 2 " + property2.frames[0].ToJson());
     }
 
     // Update is called once per frame
@@ -257,44 +305,44 @@ public class PlaybackGameplay : MonoBehaviour
          
     }
 
-    public void loadFrames()
-    {
-        frames = new List<FrameData>();
+    //public void loadFrames()
+    //{
+    //    frames = new List<FrameData>();
 
-        string filePath = Application.persistentDataPath + "/savedFrames" + "test1" + ".save";
+    //    string filePath = Application.persistentDataPath + "/savedFrames" + "test1" + ".save";
 
-        foreach (string line in File.ReadLines(filePath))
-        {
-            //System.Console.WriteLine(line);
-            //counter++;
-            // get type
-            int i0 = line.IndexOf("\"type\"");
-            int i1 = line.IndexOf(",", i0);
-            //System.Type type = (System.Type)line.Substring(i0, i1 - i0);
-            System.Type type = System.Type.GetType( line.Substring(i0, i1 - i0));
+    //    foreach (string line in File.ReadLines(filePath))
+    //    {
+    //        //System.Console.WriteLine(line);
+    //        //counter++;
+    //        // get type
+    //        int i0 = line.IndexOf("\"type\"");
+    //        int i1 = line.IndexOf(",", i0);
+    //        //System.Type type = (System.Type)line.Substring(i0, i1 - i0);
+    //        System.Type type = System.Type.GetType( line.Substring(i0, i1 - i0));
 
-            frames.Add((FrameData)JsonUtility.FromJson(line, type));
-            //frames.Add(System.Convert.ChangeType(JsonUtility.FromJson(line, type), type);
-        }
-    }
+    //        frames.Add((FrameData)JsonUtility.FromJson(line, type));
+    //        //frames.Add(System.Convert.ChangeType(JsonUtility.FromJson(line, type), type);
+    //    }
+    //}
 
-    public void saveFrames()
-    {
-        string filePath = Application.persistentDataPath + "/savedFrames" + "test1" + ".save";
+    //public void saveFrames()
+    //{
+    //    string filePath = Application.persistentDataPath + "/savedFrames" + "test1" + ".save";
 
-        File.WriteAllText(filePath, "");  // overwrite file
+    //    File.WriteAllText(filePath, "");  // overwrite file
 
-        using (StreamWriter sw = File.AppendText(filePath))
-        {
-            //sw.WriteLine("This is the new text");
-            foreach(FrameData frame in frames)
-            {
-                sw.WriteLine(frame.ToJson());
-            }
-        }
-    }
+    //    using (StreamWriter sw = File.AppendText(filePath))
+    //    {
+    //        //sw.WriteLine("This is the new text");
+    //        foreach(FrameData frame in frames)
+    //        {
+    //            sw.WriteLine(frame.ToJson());
+    //        }
+    //    }
+    //}
 
-    public void saveFile()
+    public void testReflection()
     {
         //{
         //    var obj = ghosts[0];
@@ -307,7 +355,7 @@ public class PlaybackGameplay : MonoBehaviour
         //}
         {
             //var obj = ghosts[0];
-            var obj = transform;
+            var obj = ghosts[0];
             print("Breaking down: " + obj);
             foreach (PropertyInfo property in obj.GetType().GetProperties())
             {
@@ -316,7 +364,9 @@ public class PlaybackGameplay : MonoBehaviour
                     //print("PROPERTY = " + property + " TYPE = " + property.PropertyType + " VALUE = " + property.GetValue(obj));
                     //print("PROPERTY = " + property + " TYPE = " + property.PropertyType + " VALUE = " + property.GetValue(obj));
                     print("PROPERTY = " + property + " VALUE = " + property.GetValue(obj));
-                } catch (System.Exception e)
+                    print("JSON " + JsonUtility.ToJson(property));
+                }
+                catch (System.Exception e)
                 {
                     //Debug.LogError("Exception thrown: " + e);
                 }
@@ -324,8 +374,13 @@ public class PlaybackGameplay : MonoBehaviour
             foreach (FieldInfo field in obj.GetType().GetFields())
             {
                 print("FIELD = " + field + " VALUE = " + field.GetValue(obj));
+                print("JSON " + JsonUtility.ToJson(field));
             }
         }
+    }
+
+    public void saveFile()
+    {
 
         /*
         // https://docs.microsoft.com/en-us/dotnet/standard/serialization/binaryformatter-security-guide uh oh. "Don't use binaryFormatter"
